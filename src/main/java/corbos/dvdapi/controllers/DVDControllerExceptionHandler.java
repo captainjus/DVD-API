40a735f0d64983c0ee77649bfce24c86c6d5e06c
Yasmine-KOUBAA <koubaa.yasmine@gmail.com>
2021-03-05 15:14:10 -0800

    added Error and ExeceptionHandler Classes

---
+++ src/main/java/corbos/dvdapi/controllers/DVDControllerExceptionHandler.java
@@ -0,0 +1,29 @@
+package corbos.dvdapi.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@ControllerAdvice // indicates the class will assist all controllers in our project
+@RestController  //class is able to serve an HTTP response on behalf of a controller
+public class DVDControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String CONSTRAINT_MESSAGE = "Could not save your item. "
+            + "Please ensure it is valid and try again.";
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public final ResponseEntity<Error> handleSqlException(
+            SQLIntegrityConstraintViolationException ex,
+            WebRequest request) {
+
+        Error err = new Error();
+        err.setMessage(CONSTRAINT_MESSAGE);
+        return new ResponseEntity<>(err, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+}
